dc880065eed536910c460c545de87d0f5e2e9eda
brianz98 <zdj519@gmail.com>
2020-07-15 17:08:52 +0100

    Update 01-OpenBabel.sh

--- 01-OpenBabel.sh
+++ 01-OpenBabel.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
 # Last edited by BZ 16:25 BST 15 July 2020
 
-obabel -:"${SMILES}" -O ${FILENAME} -p 7.4 --gen3d --partialcharge gasteiger
+IFS=","
+while read NAME SCORE SMILES SA
+do
+   if [[ $SA -lt 4 ]]
+   then
+      FILENAME="${NAME}.pdbqt"
+      obabel -:"${SMILES}" -O ${FILENAME} -p 7.4 --gen3d --partialcharge gasteiger
+   fi
+done
